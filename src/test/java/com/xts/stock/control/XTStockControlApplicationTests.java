2bf6e92161024704506f625143a9496cbb7d4ac6
Gabriel Souza <gabrielebep35@gmail.com>
2023-11-26 00:38:08 -0300

    Removing test application class

--- src/test/java/com/xts/stock/control/XTStockControlApplicationTests.java
+++ src/test/java/com/xts/stock/control/XTStockControlApplicationTests.java
@@ -3,7 +3,7 @@ package com.xts.stock.control;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class XTStockControlApplicationTests {
 
 	@Test
